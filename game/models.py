66921312158642bd7bb5464bb95b3830a6c7e9c2
Jung-woo Lee <jwl@Jung-wooui-MacBook.local>
2019-05-09 22:03:08 +0900

    model 세팅

--- game/models.py
+++ game/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class animal(models.Model):
+    kind=models.CharField(max_length=200)
+    status=models.IntegerField()
+    victory=models.BooleanField(default=True)
+    location=models.CharField(max_length=200)
+    limited_location=models.CharField(max_length=200)
+    life=models.BooleanField(default=True)
