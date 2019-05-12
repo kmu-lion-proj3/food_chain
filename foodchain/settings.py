5f5062c6e29bf6605c585e0e7bf78332ead571a9
HyuuunjuKim <gus7wn@gamil.com>
2019-05-12 14:48:26 +0900

    before pull

--- foodchain/settings.py
+++ foodchain/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'foodchain.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['foodchain/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
