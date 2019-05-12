1b22d7874fdd2997c1404258564dd1d2273c1991
Jung-woo Lee <jwl@Jung-wooui-MacBook.local>
2019-05-12 21:15:48 +0900

    choose

--- game/urls.py
+++ game/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('role/',  views.role, name="role"),
     path('choose_area/', views.choose_area, name="choose_area"),
+    path('choose_process/',views.choose_process,name="choose_process"),
     path('area_people/', views.area_people, name="area_people"),
     path('result/', views.result, name="result"),
     path('signup/',  views.signup, name='signup'),
